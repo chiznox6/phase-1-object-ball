cc8a55adfce18d82c5616fefd6d5477d2b52397f
James <james.kariuki5@student.moringaschool.com>
2025-06-15 12:54:22 +0300

    initial commit

--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -1,28 +1,132 @@
-console.log('Advanced debugging example running.')
-debugger
+function numPointsScored(playerName) {
+  const game = gameObject();
+  for (let team in game) {
+    let players = game[team].players;
+    if (players[playerName]) return players[playerName].points;
+  }
+}
 
-// first, define the function.
-function goodPractices() {
-  let game = gameObject();
-  for (let gameKey in game) {
-    // are you ABSOLUTELY SURE what 'gameKey' is?
-    // use the debugger to find out!
-    debugger
-    let teamObj = game[gameKey]
-    for (let teamKey in teamObj) {
-      // are you ABSOLUTELY SURE what 'teamKey' is?
-      // use debugger to find out!
-      debugger
+function shoeSize(playerName) {
+  const game = gameObject();
+  for (let team in game) {
+    let players = game[team].players;
+    if (players[playerName]) return players[playerName].shoe;
+  }
+}
 
-      // what is 'data' at each loop through out this block?
-      // when will the following line of code work and when will it break?
-      let data = teamObj.player
-      for (let key in data) {
-        debugger
-      }
+function teamColors(teamName) {
+  const game = gameObject();
+  for (let team in game) {
+    if (game[team].teamName === teamName) return game[team].colors;
+  }
+}
+
+function teamNames() {
+  const game = gameObject();
+  return [game.home.teamName, game.away.teamName];
+}
+
+function playerNumbers(teamName) {
+  const game = gameObject();
+  for (let team in game) {
+    if (game[team].teamName === teamName) {
+      return Object.values(game[team].players).map(p => p.number);
     }
   }
 }
 
-// then, call the function so it runs!
-goodPractices()
+function playerStats(playerName) {
+  const game = gameObject();
+  for (let team in game) {
+    let players = game[team].players;
+    if (players[playerName]) return players[playerName];
+  }
+}
+
+function bigShoeRebounds() {
+  const game = gameObject();
+  let biggestShoe = 0;
+  let rebounds = 0;
+
+  for (let team in game) {
+    for (let player in game[team].players) {
+      let stats = game[team].players[player];
+      if (stats.shoe > biggestShoe) {
+        biggestShoe = stats.shoe;
+        rebounds = stats.rebounds;
+      }
+    }
+  }
+
+  return rebounds;
+}
+
+// Bonus
+
+function mostPointsScored() {
+  let game = gameObject();
+  let maxPoints = 0;
+  let topPlayer = "";
+
+  for (let team in game) {
+    for (let player in game[team].players) {
+      let pts = game[team].players[player].points;
+      if (pts > maxPoints) {
+        maxPoints = pts;
+        topPlayer = player;
+      }
+    }
+  }
+
+  return topPlayer;
+}
+
+function winningTeam() {
+  const game = gameObject();
+  let homePoints = 0;
+  let awayPoints = 0;
+
+  for (let player in game.home.players) {
+    homePoints += game.home.players[player].points;
+  }
+
+  for (let player in game.away.players) {
+    awayPoints += game.away.players[player].points;
+  }
+
+  return homePoints > awayPoints ? game.home.teamName : game.away.teamName;
+}
+
+function playerWithLongestName() {
+  const game = gameObject();
+  let longest = "";
+
+  for (let team in game) {
+    for (let player in game[team].players) {
+      if (player.length > longest.length) {
+        longest = player;
+      }
+    }
+  }
+
+  return longest;
+}
+
+function doesLongNameStealATon() {
+  const game = gameObject();
+  let longestName = playerWithLongestName();
+  let maxSteals = 0;
+  let topStealer = "";
+
+  for (let team in game) {
+    for (let player in game[team].players) {
+      let steals = game[team].players[player].steals;
+      if (steals > maxSteals) {
+        maxSteals = steals;
+        topStealer = player;
+      }
+    }
+  }
+
+  return longestName === topStealer;
+}
